4870675b4e4a3fecd06c3b354065e283d963b494
hunzsig <mzyhaohaoren@qq.com>
2020-09-06 23:23:40 +0800

    feat(ring): 光环技能的补充

--- lib/skill/ring.lua
+++ lib/skill/ring.lua
@@ -54,6 +54,47 @@ hring.insert = function(whichUnit, abilId)
                         for _, m in ipairs(hmatcher.RING_MATCHER) do
                             local s, e = string.find(name, m[1])
                             if (s ~= nil and e ~= nil) then
+                                local Area1 = slk.Area1 or 0
+                                local targs1 = slk.targs1 or nil
+                                if (Area1 > 0 and type(targs1) == string) then
+                                    local radius = math.floor(Area1)
+                                    local targets = string.explode(',', targs1)
+                                    local g = hgroup.createByUnit(u, radius, function(filterUnit)
+                                        local selector = {
+                                            air = false, --空中
+                                            ground = false, --地面
+                                            structure = false, --建筑
+                                            debris = false, --残骸
+                                            organic = false, --有机的
+                                            mechanical = false, --机械的
+                                            vuln = false, --可攻击
+                                            invu = false, --无敌
+                                            friend = false, --友军
+                                            enemies = false, --敌人
+                                            allies = false, --联盟
+                                            neutral = false, --中立
+                                            self = false, --自己
+                                            nonself = false, --非自己
+                                            alive = false, --存活
+                                            dead = false, --死亡
+                                            hero = false, --英雄
+                                            nonhero = false, --非英雄
+                                            ancient = false, --古树
+                                            nonancient = false, --非古树
+                                            ward = false, --守卫
+                                        }
+                                        for _, t in ipairs(targets) do
+                                            if (selector[t] ~= nil) then
+                                                selector[t] = true
+                                            end
+                                        end
+
+                                        return status and heroStatus
+                                    end)
+                                    if (#g > 0) then
+                                        g = nil
+                                    end
+                                end
                                 m[2]({
                                     centerUnit = u,
                                 })
