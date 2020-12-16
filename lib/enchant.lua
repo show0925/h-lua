8cba3cdfbb82aff1df3bdac4db244206daeade30
hunzsig <mzyhaohaoren@qq.com>
2020-12-16 22:43:44 +0800

    feat(enchant): 附魔功能补充

--- lib/enchant.lua
+++ lib/enchant.lua
@@ -4,24 +4,60 @@ henchant = {
     -- 附魔属性存在时的固有加成，如火攻击对无火抵抗的自带10%伤害
     INTRINSIC_ADDITION = 10, --(%)
     -- 是否开启着身附魔后果
-    APPEND_OPEN = false,
+    STATUS = false,
     -- 开启着身附魔后,默认附身时间(秒)
     APPEND_DURING = 5.0,
+    -- 附魔环境附着特效
+    ENV_APPEND_EFFECT = {},
+    -- 附魔环境反应
+    ENV_RESPONSE = {},
 }
 
 --- 设置附魔的底层固有加成
+---@param percent number 加成比例(%)
 henchant.setIntrinsicAddition = function(percent)
-    henchant.INTRINSIC_ADDITION = math.round(percent)
+    if (type(percent) == 'number') then
+        henchant.INTRINSIC_ADDITION = math.round(percent)
+    end
 end
 
---- 开启着身附魔
-henchant.openAppend = function()
-    henchant.APPEND_OPEN = true
+--- 设置着身附魔
+---@param status boolean 开关附魔
+henchant.setAppend = function(status)
+    if (type(status) == 'boolean') then
+        henchant.STATUS = status
+    end
 end
 
---- 关闭着身附魔
-henchant.closeAppend = function()
-    henchant.APPEND_OPEN = false
+--- 设置单位附着附魔特效
+---@param whichEnchant string CONST_ENCHANT 对应的附魔
+---@param attach table 特效绑定的多个位置，如 {'origin', 'head'}
+---@param effect string 具体特效字符串，如 'Abilities\\Spells\\Other\\BreathOfFire\\BreathOfFireDamage.mdl'
+henchant.setAppendAttachEffect = function(whichEnchant, attach, effect)
+    if (type(whichEnchant) ~= 'string' or type(attach) ~= 'table' or type(effect) ~= 'string') then
+        return
+    end
+    heffect.del(henchant.ENV_APPEND_EFFECT[whichEnchant].object)
+    henchant.ENV_APPEND_EFFECT[whichEnchant] = {
+        object = nil,
+        attach = attach,
+        effect = effect,
+    }
+end
+
+--- 设置环境附魔反应
+---@alias setResponse fun(evtData: {triggerUnit:"触发单位",targetUnit:"目标单位"}):void
+---@param onEnchant string CONST_ENCHANT [运行时]单位附着的新的附魔
+---@param toEnchant string CONST_ENCHANT [运行时]单位已有的目标附着附魔
+---@param response setResponse  | "function(evtData) end" 新->旧 [运行时]化学反应，反应后该两个类型的附魔都将消失
+henchant.setEnvResponse = function(onEnchant, toEnchant, response)
+    if (type(onEnchant) ~= 'string' or type(toEnchant) ~= 'string' or type(response) ~= 'function') then
+        return
+    end
+    if (henchant.ENV_RESPONSE[onEnchant] == nil) then
+        henchant.ENV_RESPONSE[onEnchant] = {}
+    end
+    henchant.ENV_RESPONSE[onEnchant][toEnchant] = response
 end
 
 --- 给目标单位添加附魔属性
@@ -31,7 +67,7 @@ henchant.append = function(options)
         options = {
             targetUnit = userdata,
             sourceUnit = userdata,
-            enchants = {'fire','water'} | "fire,water",
+            enchants = {'fire','water'} | "fire,water", 建议用table
             during = 5,
         }
     ]]
@@ -39,7 +75,10 @@ henchant.append = function(options)
     local sourceUnit = options.sourceUnit
     local enchants = options.enchants
     local during = options.during or henchant.APPEND_DURING
-    if (henchant.APPEND_OPEN ~= true or during <= 0) then
+    if (during < 0) then
+        during = 0
+    end
+    if (henchant.STATUS ~= true) then
         return
     end
     if (targetUnit ~= nil and his.deleted(targetUnit) == false) then
@@ -48,14 +87,17 @@ henchant.append = function(options)
     if (sourceUnit ~= nil and his.deleted(sourceUnit) == false) then
         return
     end
-    if (type(enchants) == 'table') then
-        enchants = string.implode(',', enchants)
+    if (type(enchants) == 'string') then
+        enchants = string.explode(',', enchants)
     end
-    if (type(enchants) ~= 'string' or during <= 0) then
-        print_err("henchant append -enchants")
-        return
-    end
+    -- 整合
+    -- 判断环境
+
     hattribute.set(targetUnit, during, {
         append_enchant = "+" .. enchants
     })
+end
+
+henchant.env = function(options)
+
 end
