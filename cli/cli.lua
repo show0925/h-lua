62baaff7f72aa940440a395b9f75fa7eb04abf21
hunzsig <mzyhaohaoren@qq.com>
2021-02-19 23:11:22 +0800

    feat(*): *

--- cli/cli.lua
+++ cli/cli.lua
@@ -1,10 +1,14 @@
 require "cliSlk"
 require "main"
-local _main_start_trg = cj.CreateTrigger()
-cj.TriggerRegisterTimerEvent(_main_start_trg, 0.1, false)
-cj.TriggerAddAction(_main_start_trg, function()
-    cj.DisableTrigger(_main_start_trg)
-    cj.DestroyTrigger(_main_start_trg)
-    _main_start_trg = nil
-    main()
-end)
+if (cj ~= nil) then
+    local t = cj.CreateTrigger()
+    cj.TriggerRegisterTimerEvent(t, 0.1, false)
+    cj.TriggerAddAction(t, function()
+        cj.DisableTrigger(t)
+        cj.DestroyTrigger(t)
+        t = nil
+        if (type(main) == "function") then
+            main()
+        end
+    end)
+end
