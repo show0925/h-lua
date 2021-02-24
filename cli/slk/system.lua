e43983ffaf365c39d385e6f4a8a365a0fe627153
hunzsig <mzyhaohaoren@qq.com>
2021-02-24 12:02:24 +0800

    feat(*): *

--- cli/slk/system.lua
+++ cli/slk/system.lua
@@ -1,2 +1,32 @@
---- h-lua内置slk
+-- h-lua内置slk
 
+--- 单位
+--- #token
+hslk_unit({
+    _parent = "ogru",
+    EditorSuffix = "#h-lua",
+    Name = "Token",
+    special = 1,
+    abilList = "Avul,Aloc",
+    upgrade = "",
+    file = ".mdl",
+    unitShadow = "",
+    collision = 0,
+    Art = "",
+    movetp = "fly",
+    moveHeight = 25.00,
+    spd = 522,
+    turnRate = 3.00,
+    moveFloor = 25.00,
+    weapsOn = 0,
+    race = "other",
+    fused = 0,
+    sight = 0,
+    nsight = 0,
+    Builds = "",
+    upgrades = "",
+})
+
+hslk_item({
+    F6TIPS.Name
+})
