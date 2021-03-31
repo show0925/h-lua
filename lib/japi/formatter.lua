c221ae98c3a645b21c63808fc47e5bc0ea7f995a
hunzsig <mzyhaohaoren@qq.com>
2021-03-31 22:11:17 +0800

    chore(h-lua): v2 跟进sdk版本

--- lib/japi/formatter.lua
+++ lib/japi/formatter.lua
@@ -467,14 +467,12 @@ local formatter = {
     EXGetUnitAbilityByIndex = function(params)
         params[2] = math.tointeger(params[2]) or 0
     end,
+    EXPauseUnit = function(params)
+        params[2] = tobool(params[2])
+    end,
     EXSetAbilityState = function(params)
-        if (type(params[2]) == 'string') then
-            params[2] = string.char2id(params[2])
-        else
-            params[2] = math.tointeger(params[2]) or 0
-        end
-        params[3] = math.tointeger(params[3]) or 0
-        params[4] = math.round(params[4], 3)
+        params[2] = math.tointeger(params[2]) or 0
+        params[3] = math.round(params[3], 3)
     end,
     EXSetBuffDataString = function(params)
         params[1] = math.tointeger(params[1]) or 0
